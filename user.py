b384253c9c6e149a2005605775efdc7546516af2
RathiAaditya <f20202191@hyderabad.bit-pilani.ac.in>
2023-04-14 22:08:49 +0530

    basic code for user

--- user.py
+++ user.py
@@ -1,18 +1,38 @@
+import hashlib
+import json
+import time
+
+
 class EHRUser:
-    def __init__(self, user_id, user_type):
-        self.user_id = user_id  # unique identifier for the user
-        # type of user (e.g., patient, physician, hospital)
-        self.user_type = user_type
+    def __init__(self, name, dob, gender, contact_details, ssn):
+        self.name = name
+        self.dob = dob
+        self.gender = gender
+        self.contact_details = contact_details
+        self.ssn = ssn
+        self.ehrs = []
 
     def view_records(self, patient_id):
-        """View EHR records of a specific patient."""
-        # Implementation logic for viewing EHR records from the blockchain
-        pass
+        """Get all electronic health records (EHRs) of the user."""
+        return self.ehrs
 
-    def add_record(self, patient_id, record):
-        """Add a new EHR record for a specific patient."""
-        # Implementation logic for adding EHR records to the blockchain
-        pass
+    def add_record(self, medical_history, allergies, immunizations, lab_results, prescriptions, patient_data):
+        """Create a new electronic health record (EHR) for the user."""
+        timestamp = time.time()
+        ehr = {
+            'timestamp': timestamp,
+            'medical_history': medical_history,
+            'allergies': allergies,
+            'immunizations': immunizations,
+            'lab_results': lab_results,
+            'prescriptions': prescriptions,
+            'patient_data': patient_data
+        }
+        ehr_hash = hashlib.sha256(json.dumps(
+            ehr, sort_keys=True).encode()).hexdigest()
+        ehr['hash'] = ehr_hash
+        self.ehrs.append(ehr)
+        return ehr_hash
 
     def update_record(self, patient_id, record_id, updated_record):
         """Update an existing EHR record of a specific patient."""
